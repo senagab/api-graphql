bf24f72beb71e72f7887fe03dbaa62d58490cd54
senagab <senagab@gmail.com>
2025-01-24 23:14:33 -0400

    feat: automized schema

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,9 +1,17 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
 
 @Resolver()
 export class AppointmentsResolver {
     @Query(() => String)
     async helloWorld() {
         return 'Hello World'
+    }
+
+    @Mutation(() => Boolean)
+    async createAppointment(@Arg('data') data: CreateAppointmentInput) { 
+        
+
+        return true
     }
 }
