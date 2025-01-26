18169d861e5b20252be4b39a4af661b0baf5205e
senagab <senagab@gmail.com>
2025-01-25 23:00:14 -0400

    feat: relationships + customer model

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,17 +1,36 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from "type-graphql";
 import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
+import { Appointment } from "../dtos/models/appointment-model";
+import { Customer } from "../dtos/models/customer-model";
 
-@Resolver()
+@Resolver(() => Appointment)
 export class AppointmentsResolver {
-    @Query(() => String)
-    async helloWorld() {
-        return 'Hello World'
+    @Query(() => [Appointment])
+    async appointments() {
+        return [
+            {
+                startsAt: new Date(),
+                endsAt: new Date(),
+            }
+        ];
     }
 
-    @Mutation(() => Boolean)
+    @Mutation(() => Appointment)
     async createAppointment(@Arg('data') data: CreateAppointmentInput) { 
-        
+        const appointment = {
+            startsAt: data.startsAt,
+            endsAt: data.endsAt,
+        }
 
-        return true
+        return appointment;
+    }
+    
+    @FieldResolver(() => Customer)
+    async customer(@Root() appointment: Appointment) {
+        console.log(appointment)
+
+        return {
+            name: 'John Doe'
+        }
     }
 }
